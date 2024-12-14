189849932b88991ef635dbb1e1f2c2077c6ecc25
Mark Ballereau <mark.b@live.fr>
2024-12-14 01:17:58 +0100

    Remove test dependencies and delete GatewayApplicationTests class

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -21,16 +21,10 @@ extra["springCloudVersion"] = "2024.0.0"
 
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
 	imports {
 		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
 	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
 }
